56de914feb7a9e130150f0578d671a5494217b85
nutszebra <nutszebra@gmail.com>
2015-09-22 06:26:56 +0900

    execute vlad

---
+++ vlad.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import Pycluster as pc
+from time import time
+
+"""
+ <unblockshaped>
+ from here: http://stackoverflow.com/questions/16873441/form-a-big-2d-array-from-multiple-smaller-2d-arrays/16873755#16873755
+ Thanks, unutbu!
+"""
+def unblockshaped(arr, h, w):
+  """
+  Return an array of shape (h, w) where
+  h * w = arr.size
+
+  If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
+  then the returned array preserves the "physical" layout of the sublocks.
+  """
+  n, nrows, ncols = arr.shape
+  return (arr.reshape(h//nrows, -1, nrows, ncols)
+          .swapaxes(1,2)
+          .reshape(h, w))
+
+def kMeans(arr, k, numberToTry = 20):
+  clusterid, error, nfound = pc.kcluster(arr, nclusters=k, transpose=0,
+                                       npass=numberToTry, method='a', dist='e')
+  centroids, _ = pc.clustercentroids(arr, clusterid=clusterid)
+  return [centroids, clusterid]
+
+def normalizeArray(arr):
+  answer = np.array(arr[0] / np.linalg.norm(arr[0]))
+  for i in range(1,len(arr)):
+    if np.linalg.norm(round(arr[i],1)) == 0.0:
+        answer = np.vstack([answer, arr[i]])
+    else:
+      answer = np.vstack([answer, arr[i] / np.linalg.norm(arr[i])])
+  return answer
+
+def vlad(dic, k, structure=(14*14,512), numberToTry=20):
+  keys = []
+  newDic = {}
+  flag = False
+  for key in dic:
+    newDic[key] = unblockshaped(dic[key].T, structure[0], structure[1])
+    if flag == True:
+      dicForkmeans = np.vstack((dicForkmeans,newDic[key]))
+    else:
+      dicForkmeans = np.array(newDic[key])
+      flag = True
+    keys.append(key)
+  print("k: " + str(k))
+  print("number of pictures: " + str(len(dic)))
+  print("vector structure for each picture: " + str(structure))
+  print("number of vectores for k-means: " + str(len(dicForkmeans)))
+  print("start k-means")
+  timeMemory = time()
+  #execute kMeans
+  result = kMeans(dicForkmeans, k, numberToTry)
+  print("finish k-means")
+  print('It took ' + str(int(time() - timeMemory)) + " secondes")
+  count = 0
+  increment = 0
+  #store vlad here
+  answer = {}
+  for clusterid in result[1]:
+    key = keys[count]
+    if not answer.has_key(key):
+      answer[key] = np.zero((k,structure(1)))
+    answer[key][clusterid] = answer[key][clusterid] + (dic[key][increment] - result[0][clusterid])
+    increment = increment + 1
+    if increment >= structure(0):
+      count = count + 1
+      increment = 0
+  return answer
