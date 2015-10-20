0a8dfff50275f1893a0595ac48074a7e994ad879
EC2 Default User <ec2-user@ip-172-31-11-20.ap-northeast-1.compute.internal>
2015-10-20 12:43:58 +0000

    modify

--- noise.py
+++ noise.py
@@ -22,14 +22,14 @@ def rotatePic(inputPath, outputPath, deg=90):
 def gaussian(inputPath, outputPath):
   im = Image.open(inputPath)
   im = changeToRGBMode(im)
-  im.filter(ImageFilter.GaussianBlur).save(outputPath)
+  im.filter(ImageFilter.BLUR).save(outputPath)
 
 #unsharpmask
 #http://pillow.readthedocs.org/en/latest/reference/ImageFilter.html
 def unsharpMask(inputPath, outputPath):
   im = Image.open(inputPath)
   im = changeToRGBMode(im)
-  im.filter(ImageFilter.UnsharpMask).save(outputPath)
+  im.filter(ImageFilter.SHARPEN).save(outputPath)
 
 def smooth(inputPath, outputPath):
   im = Image.open(inputPath)
@@ -37,18 +37,22 @@ def smooth(inputPath, outputPath):
   im.filter(ImageFilter.SMOOTH).save(outputPath)
 
 def crop(inputPath, outputPath):
-  im = Image.open(inputPath)
-  im = changeToRGBMode(im)
-  box = (int(im.size[0]*0.2*random()),int(im.size[1]*0.2*random()),int(im.size[0]*0.8),int(im.size[1]*0.8))
-  im.crop(box).save(outputPath)
+  try:
+    im = Image.open(inputPath)
+    im = changeToRGBMode(im)
+    box = (int(im.size[0]*0.2*random()),int(im.size[1]*0.2*random()),int(im.size[0]*0.8),int(im.size[1]*0.8))
+    im.crop(box).save(outputPath)
+  except:
+    print("fail: " + inputPath)
 
 
 if __name__ == '__main__':
-  base = "test"
+  base = "unsplash"
   picturePath = [picture for picture in os.listdir(base)
                if re.findall(r"\.png$|\.jpg$|\.JPG$|\.PNG$|\.JPEG$",picture)]
   #create folder for pics
-  sets = {"90": rotatePic, "G":gaussian, "US":unsharpMask, "S":smooth, "C1": crop, "C2": crop, "C3":crop}
+  #sets = {"90": rotatePic, "G":gaussian, "US":unsharpMask, "S":smooth, "C1": crop, "C2": crop, "C3":crop}
+  sets = {"C1": crop}
   for key in sets:
     for picture in picturePath:
       if not os.path.exists(base + key):
